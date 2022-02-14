d98b6c27e12e3d25e1838785b636a1e41f87e467
Jhonatanslopes <jhonatans.ti@icloud.com>
2022-02-14 20:28:08 -0300

    add api

--- src/handler.py
+++ src/handler.py
@@ -2,45 +2,53 @@ import pickle
 import pandas as pd
 from class_.CarInsurance import CarInsurance
 from flask import Flask, request, Response
+import os
 
 # loading model
-path = 'C:/Users/Jhonatans/projects/ML/Classification/Health-Insurance-Cross-Sell/'
-model = pickle.load(open(path + 'src/model/xgboost.pkl', 'rb'))
+model = pickle.load(open('model/xgboost.pkl', 'rb'))
 
 # Initialize API
 app = Flask(__name__)
 
+
 @app.route('/carInsurance/predict', methods=['POST'])
 def carInsurance_predict():
-    
+
     test_json = request.get_json()
-    
-    if test_json: # There is data
-        if isinstance(test_json, dict): # unique exemple
+
+    if test_json:  # There is data
+        if isinstance(test_json, dict):  # unique exemple
             test_raw = pd.DataFrame(test_json, index=[0])
-            
-        else: # multiple example
+
+        else:  # multiple example
             test_raw = pd.DataFrame(test_json, columns=test_json[0].keys())
-            
+
         # Instantiate Class
         pipeline = CarInsurance()
-        
+
         # data cleaning
         df1 = pipeline.cleaning(test_raw)
-        
+        print('cleaning ok')
+
         # feature engineering
         df2 = pipeline.feature_engineering(df1)
-        
+        print('feature engineering ok')
+
         # data preparation
         df3 = pipeline.preparation(df2)
-        
+        print('preparation ok')
+
         # prediction
-        df_response = pipeline.get_prediction(model, test_raw, df3)
-        
+        df_response = pipeline.get_prediction(
+            model=model, original_data=df1, test_data=df3)
+        print("prediction ok")
+
         return df_response
-    
+
     else:
-        return Response('{}', status=200, mimetype='application/json')
-    
-if __name__ =='__main__':
-    app.run('0.0.0.0', debug=True)
+        return Response('{}', status=200, minetype='application/json')
+
+
+if __name__ == '__main__':
+    porta = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0', port=porta)
